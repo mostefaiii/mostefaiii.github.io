de6a082f2adc73fa2c0b07cd7f30abe185504e84
Meriem Mostefai <mostefaimeriem@gmail.com>
2025-01-31 01:48:17 -0500

    fixed tailwind issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Header = () => {
     return (
-      <button className="px-4 py-2 bg-transparent text-white font-coustard">
+      <div className="px-4 py-2 bg-transparent text-black font-coustard">
         me when i LIE
-      </button>
+      </div>
     );
   };
   
